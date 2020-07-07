0c704353c5843dc588e9ad6128a92e92318699b7
PLM <850003188@qq.com>
2020-07-07 17:24:16 +0800

    BlockingQueue接口的实现类SynchronousQueue

---
+++ src/main/java/com/plm/test/SynchronousQueueDemo.java
@@ -0,0 +1,59 @@
+package com.plm.test;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.TimeUnit;
+
+/**
+ *  BlockingQueue接口的实现类SynchronousQueue
+ *      不存储元素的阻塞队列，即单个元素的队列
+ *      米一个put操作必须要等待一个take操作，否则不能继续添加元素
+ */
+public class SynchronousQueueDemo {
+
+    public static void main(String[] args) {
+        BlockingQueue<String> blockingQueue = new SynchronousQueue<>();
+
+        new Thread(() -> {
+            try {
+                System.out.println(Thread.currentThread().getName()+"\t put 1");
+                blockingQueue.put("1");
+
+                System.out.println(Thread.currentThread().getName()+"\t put 2");
+                blockingQueue.put("2");
+
+                System.out.println(Thread.currentThread().getName()+"\t put 3");
+                blockingQueue.put("3");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        },"AA").start();
+
+        new Thread(() -> {
+            try {
+                try {
+                    TimeUnit.SECONDS.sleep(5);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(Thread.currentThread().getName()+"\t take "+blockingQueue.take());
+
+                try {
+                    TimeUnit.SECONDS.sleep(5);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(Thread.currentThread().getName()+"\t take "+blockingQueue.take());
+
+                try {
+                    TimeUnit.SECONDS.sleep(5);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(Thread.currentThread().getName()+"\t take "+blockingQueue.take());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        },"BB").start();
+    }
+}
