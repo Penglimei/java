507337d1433e163407fed63be10f572ef09e4dff
PLM <850003188@qq.com>
2020-07-28 10:57:27 +0800

    LRU缓存机制 双向链表 + HashMap

--- src/main/java/com/plm/test/LRUCacheDemo.java
+++ src/main/java/com/plm/test/LRUCacheDemo.java
@@ -14,9 +14,92 @@ import java.util.*;
  *  写入数据 put(key, value) - 如果密钥不存在，则写入其数据值。当缓存容量达到上限 capacity 时，
  *  它应该在写入新数据之前删除最久未使用的数据值，从而为新的数据值留出空间。
  */
-
 public class LRUCacheDemo {
-    // 利用 双向链表LinkedList + HashMap
+    // 利用 双向链表 + HashMap
+
+    // Node类用于抽象链表的节点
+    class Node{
+        int key;
+        int val;
+        Node pre;
+        Node next;
+    }
+
+    // 使用HashMap缓存Node节点
+    private Map<Integer,Node> map = new HashMap<>();
+    private int capacity;
+    private Node head,tail;
+
+    public LRUCacheDemo(int capacity) {
+        this.capacity = capacity;
+
+        head = new Node();
+        head.pre = null;
+        tail = new Node();
+        tail.next = null;
+        head.next = tail;
+        tail.pre = head;
+    }
+
+    // 链表头部插入新节点---->代表最近被使用的节点
+    public void addToHead(Node node){
+        node.pre = head;
+        node.next = head.next;
+        head.next.pre = node;
+        head.next = node;
+    }
+    // 删除链表中的一个节点
+    public void removeNode(Node node){
+        Node before = node.pre;
+        Node after = node.next;
+        before.next = after;
+        after.pre = before;
+    }
+
+
+    // 将节点移动到有效节点头部(head、tail都不是有效节点)---->代表LRU中节点最近被使用
+    public void moveToHead(Node node){
+        removeNode(node);
+        addToHead(node);
+    }
+    // 删除链表中最后一个有效节点(head、tail都不是有效节点)---->代表LRU队列满，删除最近未使用的节点
+    public Node popTail(){
+        Node res = tail.pre;
+        removeNode(res);
+        return res;
+    }
+
+    // LRU获取key对应的value
+    public int get(int key){
+        Node node = map.get(key);
+        if(node == null){
+            return -1;
+        }
+        moveToHead(node);
+        return node.val;
+    }
+    // LRU插入key,value
+    public void put(int key,int value){
+        Node node = map.get(key);
+        if(node == null){
+            Node newNode = new Node();
+            newNode.key = key;
+            newNode.val = value;
+            map.put(key,newNode);
+            addToHead(newNode);
+            if(map.size() > capacity){
+                Node last = popTail();
+                map.remove(last.key);
+            }
+        }else{
+            node.val = value;
+            moveToHead(node);
+        }
+    }
+}
+
+class LRUCacheDemo1 {
+    // 利用 LinkedList + HashMap
 
     // 用于存放数据
     private Map<Integer,Integer> map;
@@ -25,7 +108,7 @@ public class LRUCacheDemo {
     // 缓存容量
     private int capacity;
 
-    public LRUCacheDemo(int capacity) {
+    public LRUCacheDemo1(int capacity) {
         map = new HashMap<>();
         list = new LinkedList<>();
         this.capacity = capacity;
@@ -55,13 +138,13 @@ public class LRUCacheDemo {
 }
 
 
-class LRUCacheDemo1 {
+class LRUCacheDemo2 {
     // 利用 LinkedHashMap
 
     private Map<Integer,Integer> map;
     private int capacity;
 
-    public LRUCacheDemo1(int capacity) {
+    public LRUCacheDemo2(int capacity) {
         map = new LinkedHashMap<>();
         this.capacity = capacity;
     }
@@ -93,17 +176,16 @@ class LRUCacheDemo1 {
 class Test{
 
     public static void main(String[] args) {
-        LRUCacheDemo1 lruCacheDemo1 = new LRUCacheDemo1(2);// 1 -1 -1 3 4
-//        LRUCacheDemo lruCacheDemo1 = new LRUCacheDemo(2);// 1 -1 -1 3 4
-        lruCacheDemo1.put(1,1);
-        lruCacheDemo1.put(2,2);
-        System.out.print(lruCacheDemo1.get(1) + " ");
-        lruCacheDemo1.put(3,3);
-        System.out.print(lruCacheDemo1.get(2) + " ");
-        lruCacheDemo1.put(4,4);
-        System.out.print(lruCacheDemo1.get(1) + " ");
-        System.out.print(lruCacheDemo1.get(3) + " ");
-        System.out.print(lruCacheDemo1.get(4) + " ");
+        LRUCacheDemo lruCacheDemo = new LRUCacheDemo(2);// 1 -1 -1 3 4
+        lruCacheDemo.put(1,1);
+        lruCacheDemo.put(2,2);
+        System.out.print(lruCacheDemo.get(1) + " ");
+        lruCacheDemo.put(3,3);
+        System.out.print(lruCacheDemo.get(2) + " ");
+        lruCacheDemo.put(4,4);
+        System.out.print(lruCacheDemo.get(1) + " ");
+        System.out.print(lruCacheDemo.get(3) + " ");
+        System.out.print(lruCacheDemo.get(4) + " ");
 
     }
 }
